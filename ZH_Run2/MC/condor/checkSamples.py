d55715f12dd227073b02b6b15cd6252b7892f6da
Alexis Kalogeropoulos <alkaloge@cern.ch>
2019-10-04 06:13:41 -0500

    latest fixes

---
+++ ZH_Run2/MC/condor/checkSamples.py
@@ -0,0 +1,15 @@
+import sys
+import glob
+import os
+
+files = glob.glob('ZHTo*')
+print("files={0:s}".format(str(files)))
+for file in files :
+    if not os.path.isdir(file) : continue
+    os.chdir(file) 
+    f_ntup = glob.glob('*.ntup')
+    f_root = glob.glob('*.root')
+    print("In {0:16s} Nntup={1:2d} Nroot={2:2d}".format(file,len(f_ntup),len(f_root)))
+    os.chdir('..')
+
+
