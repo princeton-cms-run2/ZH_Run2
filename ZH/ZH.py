6c4a082e1e18c26af9ebd744d9a8de31f92f7c69
Daniel Marlow <marlow@princeton.edu>
2020-01-08 18:11:20 -0600

    Add di-lepton triggers back in.  Apply trigger selection outside of ee/mm loop.

--- ZH/ZH.py
+++ ZH/ZH.py
@@ -173,11 +173,23 @@ for count, e in enumerate(inTree) :
             for cat in cats: cutCounter[cat].count('Unique') 
         else :
             continue
-        
+
+    if args.year == 2016 :
+        if not (e.HLT_Ele27_eta2p1_WPTight_Gsf or e.HLT_Ele23_Ele12_CaloIdL_TrackIdL_IsoVL_DZ
+                or e.HLT_Ele25_eta2p1_WPTight_Gsf or e.HLT_IsoMu24 or e.HLT_IsoTkMu24 or e.HLT_Mu17_TrkIsoVVL_Mu8_TrkIsoVVL_DZ
+                or e.HLT_Mu17_TrkIsoVVL_TkMu8_TrkIsoVVL_DZ or e.HLT_IsoMu27) : continue
+
+    if (args.year == 2017 or args.year == 2018) :
+        if not (e.HLT_IsoMu24 or e.HLT_IsoMu27 or e.HLT_Mu17_TrkIsoVVL_Mu8_TrkIsoVVL_DZ_Mass8
+                or e.HLT_Mu17_TrkIsoVVL_Mu8_TrkIsoVVL_DZ_Mass3p8 or e.HLT_Ele23_Ele12_CaloIdL_TrackIdL_IsoVL_DZ 
+                or e.HLT_Ele23_Ele12_CaloIdL_TrackIdL_IsoVL and not e.HLT_Ele35_WPTight_Gsf and not e.HLT_Ele32_WPTight_Gsf ) : continue
+
+    for cat in cats: 
+	cutCounter[cat].count('Trigger')
+	if  MC :   cutCounterGenWeight[cat].countGenWeight('Trigger', e.genWeight)
+            
     for lepMode in ['ee','mm'] :
         if args.category != 'none' and not lepMode in args.category : continue
-
-        #if e.nTau < 1 : continue why this is there ? 
 
         if lepMode == 'ee' :
             if e.nElectron < 2 : continue
@@ -199,9 +211,7 @@ for count, e in enumerate(inTree) :
 
         
         if lepMode == 'ee' :
-            #if  args.year == 2016 and not e.HLT_Ele27_eta2p1_WPTight_Gsf and not e.HLT_Ele23_Ele12_CaloIdL_TrackIdL_IsoVL_DZ and not e.HLT_Ele25_eta2p1_WPTight_Gsf: continue
-            #if (args.year == 2017 or args.year == 2018) and not e.HLT_Ele23_Ele12_CaloIdL_TrackIdL_IsoVL_DZ and not e.HLT_Ele23_Ele12_CaloIdL_TrackIdL_IsoVL and not e.HLT_Ele35_WPTight_Gsf and not e.HLT_Ele32_WPTight_Gsf:
-
+            ''' 
             passCut = True 
             if  args.year == 2016 and not e.HLT_Ele27_eta2p1_WPTight_Gsf and not e.HLT_Ele25_eta2p1_WPTight_Gsf: passCut = False 
             if (args.year == 2017 or args.year == 2018) and not e.HLT_Ele35_WPTight_Gsf and not e.HLT_Ele32_WPTight_Gsf: passCut = False
@@ -211,11 +221,11 @@ for count, e in enumerate(inTree) :
                     GF.printEvent(e)
                     if MC : GF.printMC(e)
                 continue
-
+           
             for cat in cats[:4]: 
 	        cutCounter[cat].count('Trigger')
 	        if  MC :   cutCounterGenWeight[cat].countGenWeight('Trigger', e.genWeight)
-                
+            '''     
             if len(goodElectronList) < 2 :
                 if unique :
                     print("GoodLeptons Fail: : Event ID={0:d} cat={1:s}".format(e.event,cat))
@@ -234,9 +244,7 @@ for count, e in enumerate(inTree) :
             
         
         if lepMode == 'mm' :
-            #if args.year == 2016 and not e.HLT_IsoMu24 and not e.HLT_IsoTkMu24 and not e.HLT_Mu17_TrkIsoVVL_Mu8_TrkIsoVVL_DZ and not e.HLT_Mu17_TrkIsoVVL_TkMu8_TrkIsoVVL_DZ and not e.HLT_IsoMu27 : continue
-            #if (args.year == 2017 or args.year == 2018) and not e.HLT_IsoMu24  and not e.HLT_IsoMu27 and not e.HLT_Mu17_TrkIsoVVL_Mu8_TrkIsoVVL_DZ_Mass8 and not e.HLT_Mu17_TrkIsoVVL_Mu8_TrkIsoVVL_DZ_Mass3p8 : continue
-
+            '''
             if not e.HLT_IsoMu24 and not e.HLT_IsoMu27 :
                 if unique :
                     print("Trigger Fail: Event ID={0:d} cat={1:s}".format(e.event,cat))
@@ -247,7 +255,7 @@ for count, e in enumerate(inTree) :
             for cat in cats[4:]: 
 	        cutCounter[cat].count('Trigger')
 	        if  MC :   cutCounterGenWeight[cat].countGenWeight('Trigger', e.genWeight)
-
+            '''
             if len(goodMuonList) < 2 : continue
             cutCounter[cat].count('GoodLeptons')
 
