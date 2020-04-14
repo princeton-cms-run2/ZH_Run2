cd79f04669a8f9bf4a796a168a8ae91bc89ddd85
Alexis Kalogeropoulos <alkaloge@cern.ch>
2020-04-14 10:13:42 -0500

    small fix to flag JME in data

--- tools/make_jme.py
+++ tools/make_jme.py
@@ -15,10 +15,11 @@ from PhysicsTools.NanoAODTools.postprocessing.modules.jme.jetmetHelperRun2 impor
 ##(isMC=True, dataYear=2016, runPeriod="B", jesUncert="Total", redojec=False, jetType = "AK4PFchs", noGroom=False)
 ##All other parameters will be set in the helper module
 #sys.args are  sys.argv[1]= isdata, , sys.argv[2]= era, sys.argv[3]=period
-jmeCorrections = createJMECorrector(sys.argv[1], str(sys.argv[2]), str(sys.argv[3]), "Total", True, "AK4PFchs", False)
+if sys.argv[1] =='False' : jmeCorrections = createJMECorrector(False, str(sys.argv[2]), str(sys.argv[3]), "Total", True, "AK4PFchs", False)
+if sys.argv[1] =='True' :  jmeCorrections = createJMECorrector(True, str(sys.argv[2]), str(sys.argv[3]), "Total", True, "AK4PFchs", False)
 
 fnames=["./inFile.root"]
 #p=PostProcessor(".",fnames,"Jet_pt>150","",[jetmetUncertainties2016(),exampleModuleConstr()],provenance=True)
-#p=PostProcessor(".",fnames,"Jet_pt>25 && abs(Jet_eta)<4.7001","",[jmeCorrections()],provenance=True)
+#p=PostProcessor(".",fnames,"Jet_pt>250 && abs(Jet_eta)<4.7001","",[jmeCorrections()],provenance=True)
 p=PostProcessor(".",fnames,"","",[jmeCorrections()],provenance=True)
 p.run()
